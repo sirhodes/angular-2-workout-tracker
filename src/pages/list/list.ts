a4d072fa026900b92d9f701ed7bb233d94f3b816
Seth Rhodes <seth@thinkpixbit.com>
2017-04-20 19:20:29 -0400

    list: working on adding some default workouts to list, enable button to next state once workouts have been chosen

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -9,10 +9,13 @@ export class ListPage {
   selectedItem: any;
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
+  workouts: Array<{workout: string}>;
+  selectedWorkouts: Array<{workout: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
+    this.selectedWorkouts = [];
 
     // Let's populate this page with some filler content for funzies
     this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
@@ -26,8 +29,23 @@ export class ListPage {
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
     }
+    this.workouts = [
+      { workout: 'Flat Bench Press'},
+      { workout: 'Incline Bench Press'},
+      { workout: 'Dumbell Incline Bench Press'},
+      { workout: 'Barbell Curl'},
+      { workout: 'Deadlift'},
+      { workout: 'Shoulder Press'},
+      { workout: 'Barbell Squat'},
+      { workout: 'Leg Press'},
+      { workout: 'Calf Raises'},
+      { workout: 'Lat Pull-Down'},
+      { workout: 'Face Pull'}
+    ];
   }
-
+  toggleItemInWorkoutList(workoutItem) {
+    this.selectedWorkouts.push(workoutItem);
+  }
   itemTapped(event, item) {
     // That's right, we're pushing to ourselves!
     this.navCtrl.push(ListPage, {
