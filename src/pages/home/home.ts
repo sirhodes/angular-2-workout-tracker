883ef5851a83cf1ae693009e4a81433221926bfe
Seth Rhodes <seth@thinkpixbit.com>
2017-04-20 19:19:51 -0400

    home: transition to add workouts page, new header

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ListPage } from '../list/list';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,9 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  transitionToAddWorkoutsPage() {
+    this.navCtrl.push(ListPage)
   }
 
 }
